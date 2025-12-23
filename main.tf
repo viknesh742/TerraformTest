8e2c0beb5a47bedf7e306dbc7919dd760aaa786e
viknesh742 <54580316+viknesh742@users.noreply.github.com>
2025-12-23 18:32:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
 resource "aws_instance" "example" {
   ami           = var.amiID
   instance_type = "t3.micro"
+  subnet_id = "subnet-0897a8c0a6ab6f14e"
+
+  vpc_security_group_ids = [
+    "sg-00922ce187a085997"
+  ]
 
   tags = {
     Name = "HelloWorld"
